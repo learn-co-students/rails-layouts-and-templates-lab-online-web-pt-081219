a5fa4e63966ce490795e958c51a79e262d1c1c4f
wifelette <LeahSilber@gmail.com>
2020-01-26 20:11:54 -0500

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class StoreAdminController < ApplicationController
+  layout 'admin'
+
+  def home; end
+
+  def orders
+    render layout: 'order_administration'
+  end
+
+  def invoice
+    render layout: false
+  end
+end
